440b8c96e11193397957fee3255022bcf4cfb3ba
Benjamin Du <duchuanlong+git@gmail.com>
2018-11-25 11:38:40 -0800

    launch rstudio before jupyterhub

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /root
-jupyterhub -f /etc/jupyterhub/jupyterhub_config.py
 
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0 &
+
+jupyterhub -f /etc/jupyterhub/jupyterhub_config.py
+
